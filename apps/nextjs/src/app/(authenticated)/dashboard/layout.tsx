55ce9b0cabd4ef1fe74d670a3eecaa74bd95e8e2
Mohammad Shehadeh <60213956+MohammadShehadeh@users.noreply.github.com>
2025-08-01 17:14:11 +0300

    fea: dashboard layout (#7)
    * feat: add sidebar layout with navigation components and search functionality
    
    * Introduced AppSidebar component with main and secondary navigation.
    * Added NavMain and NavSecondary components for structured navigation.
    * Implemented NavUser component for user profile and dropdown menu.
    * Created SearchForm component for search functionality within the sidebar.
    * Developed SiteHeader component to include a search form and sidebar toggle.
    * Established layout for the authenticated dashboard, integrating the sidebar and header.
    
    * update dashboard layout to use functional component and pass children prop
    
    * fix: update UI components to use tailwind v3 classname

---
+++ apps/nextjs/src/app/(authenticated)/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import { SidebarInset, SidebarProvider } from "@lms/ui/components/sidebar";
+
+import { AppSidebar } from "../components/app-sidebar";
+import { SiteHeader } from "../components/site-header";
+
+export const description = "A sidebar with a header and a search form.";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="[--header-height:calc(theme(spacing.14))]">
+      <SidebarProvider className="flex flex-col">
+        <SiteHeader />
+        <div className="flex flex-1">
+          <AppSidebar />
+          <SidebarInset>
+            <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
+}
