9e4e2639544522c2e63d3b6ab75d76873e809358
Mohammad Shehadeh <mohammadzaidsh@outlook.com>
2025-11-30 00:23:24 +0300

    chore: update dependencies and configurations across the project
    - Upgraded various dependencies in package.json files, including @turbo/gen, lint-staged, turbo, and others to their latest versions.
    - Updated the pnpm-lock.yaml to reflect the changes in dependency versions.
    - Adjusted the .vscode/settings.json to specify the TypeScript SDK path.
    - Made minor adjustments in the drizzle.config.ts to utilize environment variables correctly.
    - Updated the redis dependency version in the cache package.
    - Enhanced the package.json files in multiple apps and packages to ensure compatibility with the latest versions of dependencies.

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from "drizzle-kit";
+import { dbEnv } from "./env";
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error("Missing POSTGRES_URL");
-}
+const env = dbEnv();
 
-const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
+const nonPoolingUrl = env.POSTGRES_URL.replace(":6543", ":5432");
 
 export default {
   schema: "./src/schema/index.ts",
